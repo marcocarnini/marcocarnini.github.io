538c842137adee1467795529d492a71d59e216ad
Marco Carnini <marcocarnini@gmail.com>
2023-11-22 07:54:36 +0100

    building in docs

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:
-	eval `ssh-agent`
-	ssh-add ~/.ssh/privato
---
+++ build.sh
@@ -0,0 +1,7 @@
+hugo
+mv public docs
+eval `ssh-agent`
+ssh-add ~/.ssh/privato
+
+git config user.name "Marco Carnini"
+git config user.email "marcocarnini@gmail.com"
